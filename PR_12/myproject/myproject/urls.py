b10c75b871dfcda5c26aca65e60312e04dffa9e0
Your GitHub Username <your-namratadaphale07@example.com>
2024-11-13 19:09:08 +0530

    12 Ass RegistrationForm

--- PR_12/myproject/myproject/urls.py
+++ PR_12/myproject/myproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for FoodRecipes project.
+URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# FoodRecipes/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls')),
+    path('', include('registration.urls')),
 ]
